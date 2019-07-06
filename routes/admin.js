0e72832601e51fc62b8338caa496b824a838cb6b
dimaldim <antraks92@gmail.com>
2019-07-06 17:40:52 +0300

    Change add product to /admin/

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></button> </form>');
+  res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></button> </form>');
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
