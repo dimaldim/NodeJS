fb6b21027a69ee8b4a5881f72727bcec5ce0ca26
dimaldim <antraks92@gmail.com>
2019-07-06 20:59:08 +0300

    Add routes dir

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></button> </form>');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
